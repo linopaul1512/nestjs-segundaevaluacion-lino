0cdf2075979c06770c98c8bc6b7bbd79a63edf86
linopaul_1512 <linobenavidesgabaldon.com>
2025-08-15 12:56:22 -0400

    cambie la conexion a inventario a users para quwe el get muestre usuarios y no productos

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: 5432,
       username: 'postgres',
       password: '1234',
-      database: 'inventario',
+      database: 'users',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // solo en desarrollo
     }),
